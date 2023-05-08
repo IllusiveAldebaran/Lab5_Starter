f2b2b3bcaaeeca742493f62acc16e3aa6a38f85c
hpdinh <haydenpdinh@gmail.com>
2023-05-07 21:02:08 -0700

    Added voices to dropdown

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,27 @@
 // explore.js
 
 window.addEventListener('DOMContentLoaded', init);
+const synth = window.speechSynthesis;
+const voiceSelect = document.querySelector("select");
+let voices = [];
+
+
+function populateVoiceList() {
+  voices = synth.getVoices();
+
+  for(let i = 0; i < voices.length; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = `${voices[i].name} (${voices[i].lang})`;
+    voiceSelect.appendChild(option);
+  }
+}
 
 function init() {
   // TODO
+  if (synth.onvoiceschanged !== undefined) {
+    synth.onvoiceschanged = populateVoiceList;
+  }
+
+  
 }
